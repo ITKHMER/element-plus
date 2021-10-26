e12c2d205c100a1ff04d81a7bcedbc31dd0af046
ITKHMER <93175803+ITKHMER@users.noreply.github.com>
2021-10-26 12:27:02 +0700

    Update km.ts
    All words and spelling have been confirmed with Khmer dictionary
    Note:
    pageHeader section : 
    title means ចំណងជើង
    BACK means ត្រលប់ក្រោយ
    So I translated based on text provided (BACK = ត្រលប់ក្រោយ)

--- packages/locale/lang/km.ts
+++ packages/locale/lang/km.ts
@@ -1,12 +1,12 @@
 export default {
-  name: 'km',
+  name: 'km', //name should be kh (from the word 'Khmer') instead of km
   el: {
     colorpicker: {
       confirm: 'យល់ព្រម',
       clear: 'លុប',
     },
     datepicker: {
-      now: 'ឥឡូវ​នេះ',
+      now: 'ឥឡូវនេះ',
       today: 'ថ្ងៃនេះ',
       cancel: 'បោះបង់',
       clear: 'លុប',
@@ -32,17 +32,17 @@ export default {
       month8: 'សីហា',
       month9: 'កញ្ញា',
       month10: 'តុលា',
-      month11: 'វិច្ឆកា',
+      month11: 'វិច្ឆិកា', //fix spelling error
       month12: 'ធ្នូ',
       // week: 'week',
       weeks: {
         sun: 'អាទិត្យ',
-        mon: 'Mon',
+        mon: 'ចន្ទ', //From official Khmer dictionary
         tue: 'អង្គារ',
         wed: 'ពុធ',
-        thu: 'ព្រ.ហ',
+        thu: 'ព្រ.ហ', //Full text is ព្រហស្បតិ៍ => abbre. ព្រហ or ព្រ.ហ
         fri: 'សុក្រ',
-        sat: 'សៅរ៏',
+        sat: 'សៅរ៍', //fix spelling error
       },
       months: {
         jan: 'មករា',
@@ -55,21 +55,21 @@ export default {
         aug: 'សីហា',
         sep: 'កញ្ញា',
         oct: 'តុលា',
-        nov: 'វិច្ឆកា',
+        nov: 'វិច្ឆិកា', //fix spelling error
         dec: 'ធ្នូ',
       },
     },
     select: {
       loading: 'កំពុងផ្ទុក',
-      noMatch: 'គ្មានទិន្ន័យដូច',
-      noData: 'គ្មានទិន្ន័យ',
+      noMatch: 'គ្មានទិន្នន័យដូច', //fix spelling error
+      noData: 'គ្មានទិន្នន័យ', //fix spelling error
       placeholder: 'ជ្រើសរើស',
     },
     cascader: {
-      noMatch: 'គ្មានទិន្ន័យដូច',
+      noMatch: 'គ្មានទិន្នន័យដូច', //fix spelling error
       loading: 'កំពុងផ្ទុក',
       placeholder: 'ជ្រើសរើស',
-      noData: 'គ្មានទិន្ន័យ',
+      noData: 'គ្មានទិន្នន័យ', //fix spelling error
     },
     pagination: {
       goto: 'ទៅកាន់',
@@ -81,7 +81,7 @@ export default {
       title: 'សារ',
       confirm: 'យល់ព្រម',
       cancel: 'បោះបង់',
-      error: 'ការបញ្ចូលមិនអនុញ្ញាត',
+      error: 'ការបញ្ចូលមិនត្រឹមត្រូវ',
     },
     upload: {
       deleteTip: 'ចុចលុបដើម្បីដកចេញ',
@@ -90,18 +90,18 @@ export default {
       continue: 'បន្ត',
     },
     table: {
-      emptyText: 'គ្មានទិន្ន័យ',
+      emptyText: 'គ្មានទិន្នន័យ',
       confirmFilter: 'យល់ព្រម',
       resetFilter: 'កំណត់ឡើងវិញ',
       clearFilter: 'ទាំងអស់',
       sumText: 'បូក',
     },
     tree: {
-      emptyText: 'គ្មានទិន្ន័យ',
+      emptyText: 'គ្មានទិន្នន័យ',
     },
     transfer: {
-      noMatch: 'គ្មានទិន្ន័យដូច',
-      noData: 'គ្មានទិន្ន័យ',
+      noMatch: 'គ្មានទិន្នន័យដូច',
+      noData: 'គ្មានទិន្នន័យ',
       titles: ['បញ្ជី ១', 'បញ្ជី ២'],
       filterPlaceholder: 'បញ្ចូលពាក្យ',
       noCheckedFormat: '{total} ធាតុ',
@@ -111,11 +111,11 @@ export default {
       error: 'FAILED', // to be translated
     },
     pageHeader: {
-      title: 'Back', // to be translated
+      title: 'ត្រលប់ក្រោយ', // title means ចំណងជើង, BACK means ត្រលប់ក្រោយ
     },
     popconfirm: {
-      confirmButtonText: 'Yes', // to be translated
-      cancelButtonText: 'No', // to be translated
+      confirmButtonText: 'ព្រម', // to be translated
+      cancelButtonText: 'មិនព្រម', // to be translated
     },
   },
 }
